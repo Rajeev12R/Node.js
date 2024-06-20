ce35c9126082a2c0245115ea99a89023bb16f243
Rajeev Ranjan <rjranjan2112@gmail.com>
2024-06-21 05:12:39 +0530

    Success Upload using multer library

--- image-upload/index.js
+++ image-upload/index.js
@@ -5,7 +5,20 @@ const multer = require("multer");
 const app = express();
 const PORT = 8000;
 
-const upload = multer({ dest: 'uploads/' })
+// const upload = multer({ dest: 'uploads/' })
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        return cb(null , "./uploads")
+    },
+    filename: function(req, file, cb){
+        return cb(null, `${Date.now()}-${file.originalname}`);
+    },
+
+});  
+
+const upload = multer({ storage: storage});
+
 const link = "http://localhost:8000"
 
 app.set("view engine", "ejs");
@@ -17,8 +30,11 @@ app.use(express.urlencoded({extended: false}));
 app.get("/", (req, res) =>{
     return res.render("homepage");
 })
-app.post("/upload", (req, res) =>{
+app.post("/upload", upload.single("profileImage"), (req, res) =>{
+    console.log(req.body);
+    console.log(req.file);
 
+    return res.redirect("/");
 })
 
 app.listen(PORT, ()=> console.log(`Server started at port ${PORT} \n ${link}`));
